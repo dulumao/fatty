53bb4fb0665ea531d3034712b833e9ff5c52a464
Yaoguais <897622703@qq.com>
2015-07-03 22:35:43 +0800

    index

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+// 定义应用目录
+define('APP_PATH','./Application/');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
+
+/**
+ * 目前只注释掉了Think.class.php、Mode/common.php中的部分代码
+ */
+
+//设置数据库配置
+$config = [
+    'DB_TYPE'               =>  'mysqli',     // 数据库类型
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_NAME'               =>  'test',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '',        // 端口
+    'DB_PREFIX'             =>  'pre_',
+];
+C($config);
+/**
+create table pre_log(
+id int(11) not null auto_increment primary key,
+data varchar(255) not null default ''
+);
+ */
+/*
+//数据库测试代码
+$log = M('log');
+for($i=0;$i<1000;++$i){
+    echo $log->add(['data'=>'this is a log message']);
+}*/
