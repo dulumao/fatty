144a6872522e5d8b62b38df8416eb7a0792d75e6
Yaoguais <897622703@qq.com>
2015-07-04 09:44:35 +0800

    fix result.txt

--- udp/client.php
+++ udp/client.php
@@ -5,6 +5,10 @@
  * 向服务器中的log表中添加日志数据
  */
 
+if($argc!==3){
+    exit("php client.php 10 100\n");
+}
+
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 $data = ['log','data'=>'this is a log message'];
 $data = serialize($data);
@@ -15,12 +19,14 @@ $total = $originLoop*$number;
 $errorNumber = 0;
 $timeList = [];
 $secondUnit = 1000000;
+$totalSecond = 0;
 while(--$loop>=0){
     for($i=0;$i<$number;++$i){
         $start = microtime(true);
         $send = socket_sendto($sock, $data, $len, 0, '127.0.0.1', 9905);
         if($send){
             $timeList[$loop][$i] = (microtime(true) - $start)*$secondUnit;
+            $totalSecond += $timeList[$loop][$i];
         }else{
             ++$errorNumber;
         }
@@ -41,25 +47,11 @@ echo "finally: ".(array_sum($loopTimeList)/$originLoop),"/{$secondUnit}s\n";
 
 sleep(10);
 $sendNumber = $total-$errorNumber;
-$totalSecond = array_sum($loopTimeList)/$originLoop;
 $result = "send:\nnumber:$total\n";
 $result .= "error:$errorNumber\nsuccess:$sendNumber\n";
 $result .= "takes:{$totalSecond}/{$secondUnit}s\n";
 $result .= "rqs:".($total/$totalSecond*$secondUnit)." n/s\n";
 $success = intval(file_get_contents('receive-count.txt'));
-$result .= "receive:\nsuccess:$success\n\n";
-$result .= "last:".(($sendNumber - $success)/$sendNumber*100)."%\n\n";
+$result .= "receive:\nsuccess:$success\n";
+$result .= "last:".(($sendNumber - $success)/$sendNumber*100)."%\n\n\n";
 file_put_contents('result.txt',$result,FILE_APPEND);
-
-/*
-client:
-error:0 loop:50 number:10000
-finally: 0.18803191184998/100000s
-finally: 0.18820786476135/100000s
-server:
-receive count:28010
-receive count:28006
-丢包率:94.4%
-
-
-*/
